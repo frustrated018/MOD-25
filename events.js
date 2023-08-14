cad9d431c2fbde37161e8f58c9a6f22d7dca6d16
frustrated018 <niloyd018@gmail.com>
2023-08-14 10:09:33 +0600

    Learned onclick and addEventListener

--- events.js
+++ events.js
@@ -1,1 +1,15 @@
-console.log('Added from the script file.');
+// One way to use onClick
+document.getElementById('btn-green-bg').onclick = function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+};
+
+// How we will use it most of the time
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+// Another thing we will use all the time
+
+document.getElementById('btn-blue-bg').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'Blue';
+});
